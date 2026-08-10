b0b12f157d2a1ff23cf34d54b59958fcd7d32402
pride167100-pixel <pride167100@gmail.com>
2026-08-10 17:14:38 +0900

    낚카이브 안드로이드 프로젝트 기본 구조 생성
    Kotlin + Jetpack Compose 기반 초기 뼈대: Gradle 설정, 앱 매니페스트, 시작 화면(MainActivity), 임시 아이콘/문자열 리소스 추가.

---
+++ app/src/main/java/com/nakcive/app/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.nakcive.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Text(
+        text = "낚카이브에 오신 것을 환영합니다",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    MaterialTheme {
+        HomeScreen()
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// 프로젝트 전체에서 사용할 플러그인 버전을 여기서 한 번만 선언합니다.
+plugins {
+    id("com.android.application") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+}
